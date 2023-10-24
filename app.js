ef0a71a78ad051601a4329ab4f7c70edb363aa63
Duy Long <96235121+DuyLong28@users.noreply.github.com>
2023-10-24 18:07:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -48,4 +48,17 @@ document.querySelector('.menu').addEventListener('click', function(event) {
     }
 });
 
+//Login
+$(document).ready(function(){
+    $('#eye').click(function(){
+        $(this).toggleClass('open');
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+        if($(this).hasClass('open')){
+            $(this).prev().attr('type', 'text');
+        }else{
+            $(this).prev().attr('type', 'password');
+        }
+    });
+});
+
 
